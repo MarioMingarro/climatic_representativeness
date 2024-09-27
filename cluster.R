f92dbd406b7e9707b074e24431953932046a05b0
Mario Mingarro <mario_mingarro@hotmail.com>
2024-09-27 19:12:13 +0200

    new

--- cluster.R
+++ cluster.R
@@ -48,60 +48,98 @@ bb <- data.frame(
 )
 
 
-# Definir un rango de valores K
-k_values <- 5:10
+# Número de clusters ----
+# Calcular la TSS (Suma total de cuadrados)
+tss <- sum((bb - colMeans(bb))^2)
+
+# Valores de K a probar
+k_values <- 1:10
 sse_values <- numeric(length(k_values))
+varianza_explicada <- numeric(length(k_values))
 
 # Ejecutar K-means para cada K y guardar la SSE
 for (k in k_values) {
   tictoc::tic()
-  kmeans_result <- kmeans(bb, centers = 10, nstart = 50)
+  kmeans_result <- kmeans(bb, centers = k, nstart = 50)
   print(k)
   tictoc::toc()
+  
   sse_values[k] <- kmeans_result$tot.withinss  # SSE
+  varianza_explicada[k] <- 1 - (sse_values[k] / tss)  # Porcentaje de varianza explicada
 }
 
 # Crear un data frame para graficar
-elbow_data <- data.frame(K = k_values, SSE = sse_values)
+elbow_data <- data.frame(K = k_values, VarianzaExplicada = varianza_explicada,  SSE = sse_values)
 
-ggplot(elbow_data, aes(x = K, y = SSE)) +
+## Varianza ----
+# Graficar el diagrama del codo
+ggplot(elbow_data, aes(x = K, y = VarianzaExplicada)) +
   geom_line() +
   geom_point() +
-  labs(title = "Diagrama del Codo", x = "Número de Clusters (K)", y = "Suma de Errores Cuadráticos (SSE)") +
-  theme_minimal()
+  labs(title = "Diagrama del Codo - Varianza Explicada", 
+       x = "Número de Clusters (K)", 
+       y = "Porcentaje de Varianza Explicada") +
+  theme_minimal() +
+  scale_y_continuous(labels = scales::percent)
+
+## SSE ----
+ggplot(elbow_data, aes(x = K, y = SEE)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Diagrama del Codo - Varianza Explicada", 
+       x = "Número de Clusters (K)", 
+       y = "Porcentaje de Varianza Explicada") +
+  theme_minimal() +
+  scale_y_continuous()
 
 # Kmeans----
-kk <- kmeans(x = bb, centers = 10, nstart = 50)
+kk <- kmeans(x = bb, centers = 6, nstart = 50)
 bb <- bb %>% mutate(cluster = kk$cluster)
-
-bb <- filter(bb, bb$cluster ==1)
-min(bb$V1)
-max(bb$V1)
 
 bb <- data.frame(
   Distancia = puntos_todos$distancia_minima,
   MH = puntos_todos$mh_guadarrama
 )
-kk <- kmeans(x = bb, centers = 2, nstart = 50)
-bb <- bb %>% mutate(cluster = kk$cluster)
 
+
+# Ver clusters----
 ggplot2::ggplot(bb, aes(x=bb$Distancia, y= bb$MH, 
                         color =  as.factor(bb$cluster)))+
   ggplot2::geom_point()+
   scale_color_brewer(palette = "RdBu") +
   geom_smooth()
 
+puntos_sp <- puntos_todos %>% mutate(bb)
 
-
-
-
-
-# 6. Convertir a un objeto 'SpatialPointsDataFrame' para el semivariograma
-puntos_sp <- as(cc, "Spatial")
-puntos_sp <- st_as_sf(puntos_sp)
+class(puntos_sp)
 
 st_write(puntos_sp, "C:/A_TRABAJO/A_GABRIEL/REPRESENTATIVIDAD/my_shapefile.shp")
 
+dentro <- filter(puntos_sp, puntos_sp$Distancia == 0)
+max(dentro$MH)
+aa <- filter(puntos_sp, puntos_sp$cluster == 1 & puntos_sp$MH <= max(dentro$MH))
 
 
 
+# Visualizar el objeto sf llamado 'aa' sobre un mapa base de relieve (Stamen Terrain)
+ggplot() +
+  
+  # Mostrar los datos de 'aa' (polígonos o puntos)
+  geom_sf(data = aa, aes(geometry = geometry), color = "blue", fill = NA, size = 1) +
+  # Título y ajustes de visualización
+  labs(title = "aaa", 
+       subtitle = "Visualización de datos sf sobre mapa") +
+  theme_minimal() +
+  coord_sf()
+
+library(tmap)
+
+# Cambiar el modo de tmap a interactivo
+tmap_mode("view")
+
+# Visualizar las CCAA con un mapa de relieve interactivo
+tm_shape(aa) +
+  tm_borders(col = "blue") +  # Mostrar los bordes de las CCAA
+  tm_fill(col = "lightblue", alpha = 0.5) +  # Rellenar las CCAA con color
+  tm_basemap() +  # Agregar mapa base de relieve
+  tm_layout(title = "Ckkkkk")
