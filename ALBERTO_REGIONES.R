b9a2a4bb0139406270fcc95b9e6922bf015868ef
Mario Mingarro <mario_mingarro@hotmail.com>
2024-10-27 17:50:38 +0100

    n

--- ALBERTO_REGIONES.R
+++ ALBERTO_REGIONES.R
@@ -67,6 +67,38 @@ toc()
 
 
 write.csv2(regiones,paste0(directorio_result, "regiones_var.csv"))
+r <- read.csv2(paste0(directorio_result, "regiones_var.csv"))
+r <- r[,-c(1,2,3,4)]
+
+HFI <- terra::rast("C:/A_TRABAJO/A_ALBERTO_JIM/VARIABLES/HFI/hfp2018.tif")
+c <- terra::crs(HFI)
+reg <- st_transform(regiones, c)
+HFI <- terra::mask(terra::crop(HFI, reg), reg)
+HFI <- project(HFI, "+proj=longlat +datum=WGS84 +no_defs")
+HFI <- raster::extract(HFI, regiones, fun=mean, na.rm=TRUE)
+regiones$HFI <- HFI$hfp2018
+
+
+NPP <- terra::rast("C:/A_TRABAJO/A_ALBERTO_JIM/VARIABLES/NPP/CHELSA_npp_1981-2010_AE.tif")
+NPP <- terra::mask(terra::crop(NPP, regiones), regiones)
+NPP <- raster::extract(NPP, regiones, fun=mean, na.rm=TRUE)
+regiones$NPP <- NPP$`CHELSA_npp_1981-2010_AE`
+
+r2 <- regiones[,c(4,13,14)]
+kk <- cbind(r,r2)
+
+write.csv2(kk,paste0(directorio_result, "regiones_var_all_N.csv"))
+
+
+TCD <- rast("C:/A_TRABAJO/A_ALBERTO_JIM/VARIABLES/TCD/TCD_2018_100m_es_25830_V1_0.tif")
+TCD <- project(TCD, "+proj=longlat +datum=WGS84 +no_defs")
+TCD <- terra::mask(terra::crop(TCD, regiones), regiones)
+TCD <- raster::extract(TCD, regiones, fun=mean, na.rm=TRUE)
+regiones$TCD <- TCD$Class_Name
+
+TCD_df <- raster::extract(TCD, regiones, fun=mean, na.rm=TRUE, df=TRUE)
+
+regiones[[TCD]] <- TCD_df$layer
 
 plot(variable)
 plot(regiones$geometry, add=T)
